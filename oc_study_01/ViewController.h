d63d26819f660592476c2cfb33cc1101c8095e5f
mac373 <mac373@mac373deiMac.local>
2015-08-27 15:40:51 +0800

    Initial Commit

---
+++ oc_study_01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  oc_study_01
+//
+//  Created by mac373 on 15/8/27.
+//  Copyright (c) 2015年 ole. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ oc_study_01/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  oc_study_01
+//
+//  Created by mac373 on 15/8/27.
+//  Copyright (c) 2015年 ole. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
